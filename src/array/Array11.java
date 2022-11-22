398775d8764b53baa4d7d16d4260521742dcd564
이동철 <choise154@gmail.com>
2022-11-22 23:23:32 +0900

    배열 11번 문제 해결

--- src/array/Array11.java
+++ src/array/Array11.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 // 11. 임시반장 정하기
 public class Array11 {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int grade = 5;
@@ -21,21 +20,19 @@ public class Array11 {
             }
         }
 
-        for(int i = 0; i<grade; i++) {
+        for(int i = 0; i<n; i++) {
             for(int j = 0; j<n; j++) {
-                for(int k = 0; k<n; k++) {
-                    if(j != k && (stu[j][i] == stu[k][i])) {
-                        answerArr[j]++;
+                for(int k = 0; k<grade; k++) {
+                    if(stu[i][k] == stu[j][k]) {
+                        answerArr[i]++;
+                        break;
                     }
                 }
             }
-        }
-
-        for(int i = 0; i<n; i++) {
             if(answerNum < answerArr[i]) {
-                answerStu = (i+1);
+                answerNum = answerArr[i];
+                answerStu = i+1;
             }
-            answerNum = answerArr[i];
         }
         System.out.println(answerStu);
     }
