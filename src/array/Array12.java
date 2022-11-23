164445e446c12ab47c726df69b925a5a842d2f8f
Dongd <choise154@gmail.com>
2022-11-23 17:46:22 +0900

    배열 12번 멘토링 문제 디버깅중

---
+++ src/array/Array12.java
@@ -0,0 +1,33 @@
+
+package array;
+
+import java.util.Scanner;
+
+// 12. 멘토링
+public class Array12 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] arr = new int[m][n];
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(i == j) { break; }
+                for(int k=0; k<m; k++) {
+                    arr[]
+                    for(int r=0; r<n; r++) {
+
+                    }
+                }
+            }
+
+        }
+    }
+}
