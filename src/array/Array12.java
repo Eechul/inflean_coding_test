675c4f9c506cf3ff1e67afcb8555676a544c494b
이동철 <choise154@gmail.com>
2022-11-25 00:13:54 +0900

    배열 12번 멘토링 문제 해결

--- src/array/Array12.java
+++ src/array/Array12.java
@@ -11,6 +11,11 @@ public class Array12 {
         int n = in.nextInt();
         int m = in.nextInt();
         int[][] arr = new int[m][n];
+        int pi = -1;
+        int pj = -1;
+        int cnt = 0;
+        int answer = 0;
+
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 arr[i][j] = in.nextInt();
@@ -19,15 +24,26 @@ public class Array12 {
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(i == j) { break; }
-                for(int k=0; k<m; k++) {
-                    arr[]
+                if(i == j) { continue; }
+                for(int t=0; t<m; t++) {
                     for(int r=0; r<n; r++) {
-
+                        if(arr[t][r] == (i+1)) {
+                            pi = (r+1);
+                        }
+                        if(arr[t][r] == (j+1)) {
+                            pj = (r+1);
+                        }
+                    }
+                    if(pi < pj) {
+                        cnt++;
                     }
                 }
+                if(cnt == m) { answer++; }
+                pi = -1;
+                pj = -1;
+                cnt = 0;
             }
-
         }
+        System.out.println(answer);
     }
 }
