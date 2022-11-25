727f44dc2f7e988285b71b6e783266c5b8878c4a
이동철 <choise154@gmail.com>
2022-11-25 12:31:16 +0900

    sliding window 4. 연속 부분수열 문제 푸는중

---
+++ src/twopointerAndSlidingwindow/ts4.java
@@ -0,0 +1,20 @@
+package twopointerAndSlidingwindow;
+
+import java.util.Scanner;
+
+// 4. 연속 부분수열
+public class ts4 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) { arr[i] = in.nextInt(); }
+
+
+    }
+
+
+}
