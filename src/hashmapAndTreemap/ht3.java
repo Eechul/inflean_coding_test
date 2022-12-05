8fbf859d3b56e2cdb9014606ab4e6a84f230dd3e
이동철 <choise154@gmaill.com>
2022-12-05 23:09:44 +0900

    3. 매출액의 종류 해결

--- src/hashmapAndTreemap/ht3.java
+++ src/hashmapAndTreemap/ht3.java
@@ -1,6 +1,7 @@
 package hashmapAndTreemap;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -10,21 +11,29 @@ public class ht3 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int k = in.nextInt();
-        int p = n % k;
         int[] arr = new int[n];
+        ArrayList<Integer> answers = new ArrayList<>();
         HashMap<Integer, Integer> map = new HashMap<>();
-        int lt = 0, rt = 1;
+        int lt = 0;
 
         for(int i=0; i<arr.length; i++) {
             arr[i] = in.nextInt();
         }
 
-        while(rt < n) {
-            if(rt - lt == k-1) { lt++;  }
-            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
-            rt++;
+        for(int j=0; j<k-1; j++) {
+            map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
         }
 
-
+        for(int rt=k-1; rt<n; rt++) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            answers.add(map.size());
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            if(map.get(arr[lt]) == 0) { map.remove(arr[lt]); }
+            lt++;
+        }
+        
+        for(Integer a : answers) {
+            System.out.printf(a+" ");
+        }
     }
 }
