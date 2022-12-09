cfa00a1ec48bbeed02f585ffc51891d81a75283c
이동철 <choise154@gmaill.com>
2022-12-09 22:56:55 +0900

    5. k번째 큰 수 해결

--- src/hashmapAndTreemap/ht5.java
+++ src/hashmapAndTreemap/ht5.java
@@ -1,8 +1,6 @@
 package hashmapAndTreemap;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 // 5. K번째 큰 수
 public class ht5 {
@@ -12,7 +10,7 @@ public class ht5 {
         int n = in.nextInt();
         int k = in.nextInt();
         int[] arr = new int[n];
-        ArrayList<Integer> list = new ArrayList<>();
+        TreeSet<Integer> treeset = new TreeSet<>(Comparator.reverseOrder());
         int answer = -1;
 
         for (int i=0; i<n; i++) { arr[i] = in.nextInt(); }
@@ -20,15 +18,14 @@ public class ht5 {
         for(int i=0; i<n-2; i++) {
             for(int j=i+1; j<n-1; j++) {
                 for(int p=j+1; p<n; p++) {
-                    System.out.println(i+" / "+j+" / "+p);
-                    list.add(arr[i] + arr[j] + arr[p]);
+                    if(!treeset.contains(arr[i] + arr[j] + arr[p])) {
+                        treeset.add(arr[i] + arr[j] + arr[p]);
+                    }
                 }
             }
         }
-        int size = list.size();
-        System.out.println("size = " + size);
-        Collections.sort(list, Collections.reverseOrder());
-        if(list.get(k-1) != null) { answer = list.get(k-1); }
-        System.out.println(""+answer);
+        Integer[] tmp = treeset.toArray(new Integer[0]);
+        if(tmp.length > k) { answer = treeset.toArray(new Integer[0])[k-1]; }
+        System.out.println(answer);
     }
 }
