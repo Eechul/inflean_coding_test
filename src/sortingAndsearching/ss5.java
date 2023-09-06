d7ff378cfe16a1c8754edd0a43edec76ab72d1a5
이동철 <choise154@gmail.com>
2023-09-06 09:11:18 +0900

    5. 중복 확인 문제에 정렬를 사용해보자

--- src/sortingAndsearching/ss5.java
+++ src/sortingAndsearching/ss5.java
@@ -1,5 +1,6 @@
 package sortingAndsearching;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 // 5. 중복 확인
@@ -15,17 +16,28 @@ public class ss5 {
         for (int i = 0; i < N; i++) {
             numbers[i] = in.nextInt();
         }
+        
+        // 내가 푼 알고리즘
+//        for (int i = 0; i < N-1; i++) {
+//             for (int j = i+1; j < N; j++) {
+//                 if (numbers[i] == numbers[j]) {
+//                     answer = 'D';
+//                     break;
+//                 }
+//             }
+//             if (answer == 'D') { break; }
+//
+//        }
 
-        for (int i = 0; i < N - 1; i++) {
-             for (int j = i+1; j < N; j++) {
-                 if (numbers[i] == numbers[j]) {
-                     answer = 'D';
-                     break;
-                 }
-             }
-             if (answer == 'D') { break; }
-
+        // 정렬을 통한 알고리즘
+        Arrays.sort(numbers);
+        for (int i = 0; i < N-1; i++) {
+            if (numbers[i] == numbers[i+1]) {
+                answer = 'D';
+                break;
+            }
         }
+
         System.out.println(answer);
     }
 }
