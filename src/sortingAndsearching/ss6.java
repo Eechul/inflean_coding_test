a568d7634586e9e71f99ad088e94d2d1e0dda997
이동철 <choise154@gmail.com>
2023-09-06 09:24:37 +0900

    6. 장난 꾸러기 문제 해결

---
+++ src/sortingAndsearching/ss6.java
@@ -0,0 +1,29 @@
+package sortingAndsearching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 6. 장난 꾸러기
+public class ss6 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int[] heights = new int[N];
+        int[] sortedHeights = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            heights[i] = in.nextInt();
+            sortedHeights[i] = heights[i];
+        }
+
+        Arrays.sort(sortedHeights);
+
+        for (int i = 0; i < N; i++) {
+            if (heights[i] != sortedHeights[i]) {
+                System.out.print(i+1+" ");
+            }
+        }
+    }
+}
