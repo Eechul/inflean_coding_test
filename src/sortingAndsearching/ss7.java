1a47655f8eadb83111e0860fb3e283bfd8679053
이동철 <choise154@gmail.com>
2023-09-07 14:06:56 +0900

    7. 좌표정렬 compareTo를 이용하여 풀기

--- src/sortingAndsearching/ss7.java
+++ src/sortingAndsearching/ss7.java
@@ -1,39 +1,39 @@
 package sortingAndsearching;
 
-import java.util.Scanner;
+import java.util.*;
 
 // 7. 좌표 정렬
 public class ss7 {
+
+    public static class Point implements Comparable<Point> {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if (this.x == o.x) { return this.y - o.y; }
+            return this.x - o.x;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         int N = in.nextInt();
-        int[][] xys = new int[N][2];
-        int[] tmp;
-
-        for (int i=0; i<N; i++) {
-            xys[i][0] = in.nextInt();
-            xys[i][1] = in.nextInt();
+        Point[] points = new Point[N];
+        
+        for (int i=0; i<N; i++) { 
+            points[i] = new Point(in.nextInt(), in.nextInt());
         }
 
-        for (int i=0; i<N-1; i++) {
-            for(int j=0; j<N-1; j++) {
-                if(xys[j][0] != xys[j+1][0] && xys[j][0] > xys[j+1][0]) {
-                    tmp = xys[j];
-                    xys[j] = xys[j+1];
-                    xys[j+1] = tmp;
-                }
-                if(xys[j][0] == xys[j+1][0] && xys[j][1] > xys[j+1][1]) {
-                    tmp = xys[j];
-                    xys[j] = xys[j+1];
-                    xys[j+1] = tmp;
-                }
-            }
-        }
+        Arrays.sort(points);
 
-        for (int i=0; i<N; i++) {
-            System.out.println(xys[i][0]+" "+xys[i][1]);
+        for (Point point : points) {
+            System.out.println(point.x+" "+point.y);
         }
     }
 }
